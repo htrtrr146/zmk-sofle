5dc9194464b40fdf93840dd7596d7f0c05f9de68
htrtrr146 <htrtrr146@gmail.com>
2025-12-06 13:39:09 +0300

    Update key matrix mapping in eylash_sofle.h

--- config/eylash_sofle.h
+++ config/eylash_sofle.h
@@ -1,12 +1,12 @@
 /*                                      60 KEY MATRIX / LAYOUT MAPPING
 
   ╭────────────────────────────┬────────────────────────────╮ ╭─────────────────────────────┬─────────────────────────────╮
-  │  0   1   2   3   4   5     │      6   7   8   9  10  11 │ │ LN5 LN4 LN3 LN2 LN1 LN0     │     RN0 RN1 RN2 RN3 RN4 RN5 │
-  │ 12  13  14  15  16  17     │     18  19  20  21  22  23 │ │ LT5 LT4 LT3 LT2 LT1 LT0     │     RT0 RT1 RT2 RT3 RT4 RT5 │
-  │ 24  25  26  27  28  29     │     30  31  32  33  34  35 │ │ LM5 LM4 LM3 LM2 LM1 LM0     │     RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 36  37  38  39  40  41  42 │ 43  44  45  46  47  48  49 │ │ LB5 LB4 LB3 LB2 LB1 LB0 LEC │ REC RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰───────╮ 50  51  52  53  54 │ 55  56  57  58  59 ╭───────╯ ╰───────╮ LH4 LH3 LH2 LH1 LH0 │ RH0 RH1 RH2 RH3 RH4 ╭───────╯
-          ╰────────────────────┴────────────────────╯                 ╰─────────────────────┴─────────────────────╯         */
+  │  0   1   2   3   4   5     │  6   7   8   9  10  11  12 │ │ LN5 LN4 LN3 LN2 LN1 LN0     │ JJU RN0 RN1 RN2 RN3 RN4 RN5 │
+  │ 13  14  15  16  17  18     │ 19  20  21  22  23  24  25 │ │ LT5 LT4 LT3 LT2 LT1 LT0     │ JJD RT0 RT1 RT2 RT3 RT4 RT5 │
+  │ 26  27  28  29  30  31     │ 32  33  34  35  36  37  38 │ │ LM5 LM4 LM3 LM2 LM1 LM0     │ JJL RM0 RM1 RM2 RM3 RM4 RM5 │
+  │ 39  40  41  42  43  44     │ 45  46  47  48  49  50  51 │ │ LB5 LB4 LB3 LB2 LB1 LB0     │ JJR RB0 RB1 RB2 RB3 RB4 RB5 │
+  ╰────╮52  53  54  55  56  57 │ 58  59  60  61  62  63 ╭───╯ ╰────╮LEC LH4 LH3 LH2 LH1 LH0 │ REC RH0 RH1 RH2 RH3 RH4 ╭───╯
+       ╰───────────────────────┴────────────────────────╯          ╰────────────────────────┴─────────────────────────╯         */
 
 #pragma once
 
@@ -17,66 +17,66 @@
 #define LN4  1
 #define LN5  0
 
-#define RN0  6  // right-number row
-#define RN1  7
-#define RN2  8
-#define RN3  9
-#define RN4 10
-#define RN5 11
+#define RN0  7  // right-number row
+#define RN1  8
+#define RN2  9
+#define RN3 10
+#define RN4 11
+#define RN5 12
 
-#define LT0 17  // left-top row
-#define LT1 16
-#define LT2 15
-#define LT3 14
-#define LT4 13
-#define LT5 12
+#define LT0 18  // left-top row
+#define LT1 17
+#define LT2 16
+#define LT3 15
+#define LT4 14
+#define LT5 13
 
-#define RT0 18  // right-top row
-#define RT1 19
-#define RT2 20
-#define RT3 21
-#define RT4 22
-#define RT5 23
+#define RT0 20  // right-top row
+#define RT1 21
+#define RT2 22
+#define RT3 23
+#define RT4 24
+#define RT5 25
 
-#define LM0 29  // left-middle row
-#define LM1 28
-#define LM2 27
-#define LM3 26
-#define LM4 25
-#define LM5 24
+#define LM0 31  // left-middle row
+#define LM1 30
+#define LM2 29
+#define LM3 28
+#define LM4 27
+#define LM5 26
 
-#define RM0 30  // right-middle row
-#define RM1 31
-#define RM2 32
-#define RM3 33
-#define RM4 34
-#define RM5 35
+#define RM0 33  // right-middle row
+#define RM1 34
+#define RM2 35
+#define RM3 36
+#define RM4 37
+#define RM5 38
 
-#define LB0 41  // left-bottom row
-#define LB1 40
-#define LB2 39
-#define LB3 38
-#define LB4 37
-#define LB5 36
+#define LB0 44  // left-bottom row
+#define LB1 43
+#define LB2 42
+#define LB3 41
+#define LB4 40
+#define LB5 39
 
-#define LEC 42  // left encoder key
-#define REC 43  // right encoder key
+#define RB0 46  // right-bottom row
+#define RB1 47
+#define RB2 48
+#define RB3 49
+#define RB4 50
+#define RB5 51
 
-#define RB0 44  // right-bottom row
-#define RB1 45
-#define RB2 46
-#define RB3 47
-#define RB4 48
-#define RB5 49
+#define LH0 57  // left thumb keys
+#define LH1 56
+#define LH2 55
+#define LH3 54
+#define LH4 53
 
-#define LH0 54  // left thumb keys
-#define LH1 53
-#define LH2 52
-#define LH3 51
-#define LH4 50
+#define RH0 59  // right thumb keys
+#define RH1 60
+#define RH2 61
+#define RH3 62
+#define RH4 63
 
-#define RH0 55  // right thumb keys
-#define RH1 56
-#define RH2 57
-#define RH3 58
-#define RH4 59
+#define LEC 52  // left encoder key
+#define REC 58  // right encoder key
